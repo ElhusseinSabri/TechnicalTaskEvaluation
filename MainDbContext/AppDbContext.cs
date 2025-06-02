40f557423c98a5e17bd9e0a3360ae82257b618c9
Elhussein <elhussein01@hotmail.com>
2025-06-02 16:25:15 +0300

    Updates

--- MainDbContext/AppDbContext.cs
+++ MainDbContext/AppDbContext.cs
@@ -15,5 +15,26 @@ namespace MainDbContext
         public DbSet<Department> Departments { get; set; }
         public DbSet<EmployeeReview> EmployeeReviews { get; set; }
 
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Department>().HasData(
+        //        new Department { Id = 1, Name = "Marketing" },
+        //        new Department { Id = 2, Name = "Development" }
+        //    );
+
+        //    modelBuilder.Entity<Employee>().HasData(
+        //        new Employee { Id = 1, Name = "Mohamed", DepartmentId = 1 },
+        //        new Employee { Id = 2, Name = "Ahmed", DepartmentId = 1 },
+        //        new Employee { Id = 3, Name = "Sarah", DepartmentId = 1 },
+        //        new Employee { Id = 4, Name = "Nahla", DepartmentId = 2 },
+        //        new Employee { Id = 5, Name = "Hanaa", DepartmentId = 2 },
+        //        new Employee { Id = 6, Name = "Soaad", DepartmentId = 2 }
+        //    );
+        //}
+
+
     }
 }
--- Models/Models/Employee.cs
+++ Models/Models/Employee.cs
@@ -11,5 +11,6 @@ namespace Models.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public Department? Department { get; set; }
+        public int DepartmentId { get; set; }
     }
 }
